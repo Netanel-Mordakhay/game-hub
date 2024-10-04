11fa0b97f2a00572ce87a436a9957e3c6c789873
Netanel <mordakhay.netanel@gmail.com>
2024-10-04 17:17:11 +0300

    Custom fetch game hook created

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]); // Stores game objects
+  const [error, setError] = useState(""); // Error messages
+
+  // Effect hook used to send fetch request to the backend
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        // If error is cancelled error, do not show error message
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
