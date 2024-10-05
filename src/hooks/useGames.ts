238d377c340394c0e5e0b6d4a39df3d59a06a917
Netanel <mordakhay.netanel@gmail.com>
2024-10-05 15:52:36 +0300

    Game card skeletons built

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,24 +24,31 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]); // Stores game objects
   const [error, setError] = useState(""); // Error messages
+  const [isLoading, setLoading] = useState(false);
 
   // Effect hook used to send fetch request to the backend
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
+
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         // If error is cancelled error, do not show error message
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
