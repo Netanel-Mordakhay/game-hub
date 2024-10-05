9b7eefe9b3697debc3149c39721f50fdd6261d9a
Netanel <mordakhay.netanel@gmail.com>
2024-10-05 18:48:53 +0300

    Genres display

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,17 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`, // ~992px
       }}
+      // lg: Aside 200px, main takes all space
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
